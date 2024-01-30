3f4bd417dcc00be8627c0efba1bf513a63d2b5c7
MooArnon <oomarnon.000@gmail.com>
2024-01-30 13:11:13 +0700

    [HOTFIX]

--- run_local_scrape.py
+++ run_local_scrape.py
@@ -6,3 +6,15 @@ scraper = BinanceScraper()
 scraper.scrape(
     assets=["BTCUSDT", "BNBUSDT"]
 )
+
+data = scraper.scrape(
+    assets=["BTCUSDT", "BNBUSDT"],
+    return_result = True,
+    result_path = "tmp_tests"
+)
+
+assert isinstance(data, list)
+
+for element in data:
+    assert isinstance(element, dict)
+    assert isinstance(float(element["price"]), float)
