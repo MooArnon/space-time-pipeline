12f37b2a9ede1473816fadf8901ae1498a891b49
MooArnon <oomarnon.000@gmail.com>
2024-11-16 16:22:11 +0700

    [discord-alert] to create method to alert at discord

--- run_local_notify.py
+++ run_local_notify.py
@@ -1,9 +1,17 @@
-import os
+from space_time_pipeline.notify.discord import DiscordNotify
 
-from space_time_pipeline.notify import LineNotify
+# Replace this with your webhook URL
+WEBHOOK_URL = "CHANGEME"
 
-notifier = LineNotify(
-    token = os.environ.get('LINE_TOKEN')
-)
+##############################################################################
 
-notifier.sent_message("test 555")
+if __name__ == "__main__":
+    
+    discord = DiscordNotify(webhook_url=WEBHOOK_URL)
+    # discord.sent_message(message = "Test from Arnon's Macbook pro")
+    discord.sent_message_image(
+        message="Test image from Arnon's Macbook pro", 
+        file_path="test.png",
+    )
+
+##############################################################################
