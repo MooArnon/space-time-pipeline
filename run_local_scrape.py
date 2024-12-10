dafe7b37b0e561ae9059c3063c292ebb4c5918b5
MooArnon <oomarnon.000@gmail.com>
2024-12-10 07:31:27 +0700

    [detailed-scraper] more detail scraper

--- run_local_scrape.py
+++ run_local_scrape.py
@@ -1,15 +1,35 @@
 from space_time_pipeline.scraper import BinanceScraper
 
-scraper = BinanceScraper()
+params = {"interval": "15m", "limit": 1}
+columns = [
+    'open_time', 'open', 'high', 'low', 'close', 'volume',
+    'close_time', 'quote_asset_volume', 'number_of_trades',
+    'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'
+]
+column_type = {
+    'open_time': 'int64',
+    'open': 'float64',
+    'high': 'float64',
+    'low': 'float64',
+    'close': 'float64',
+    'volume': 'float64',
+    'close_time': 'int64',
+    'quote_asset_volume': 'float64',
+    'number_of_trades': 'int64',
+    'taker_buy_base_asset_volume': 'float64',
+    'taker_buy_quote_asset_volume': 'float64',
+    'ignore': 'object'
+}
 
-data = scraper.scrape(
-    assets=["BTCUSDT", "BNBUSDT"],
-    return_result = True,
-    result_path = "tmp_scrape"
+scraper = BinanceScraper(
+    key="https://fapi.binance.com/fapi/v1/klines",
 )
 
-assert isinstance(data, list)
+data = scraper.detail_scrape_to_pd(
+    assets=["BTCUSDT", "BNBUSDT", "ADAUSDT", "ETHUSDT", "DOGEUSDT", "XRPUSDT"],
+    params=params,
+    columns=columns,
+    column_type=column_type,
+)
 
-for element in data:
-    assert isinstance(element, dict)
-    assert isinstance(float(element["price"]), float)
+data.to_csv("test-scraper.csv")
