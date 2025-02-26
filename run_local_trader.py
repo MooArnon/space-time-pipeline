c11cf219e2a1918ddfe980891954441c25e1b9b5
MooArnon <oomarnon.000@gmail.com>
2025-02-26 23:23:39 +0700

    [hold-same-position] adding checking position

--- run_local_trader.py
+++ run_local_trader.py
@@ -14,22 +14,23 @@ print(f"Available Balance: {available_balance}")
 trader.client.futures_account_balance
 """
 trader.create_order(
-    symbol="ETHUSDT",
-    position_type="SHORT",
-    tp_percent=5,
+    symbol="BTCUSDT",
+    position_type="LONG",
+    tp_percent=10,
     sl_percent=5,
-    leverage=30,
+    leverage=8,
 )
-
-
-trader.close_all_positions("ETHUSDT")
 """
+# trader.close_all_positions("ETHUSDT")
 
+
+"""
 trader.check_and_create_stop_loss(
     "ETHUSDT",
     stop_loss_percent = 0.01,
     max_stop_loss_percent = 15,
 )
+"""
 
-
-print(trader.daily_pnl)
+position = trader.check_position_opening(symbol="BTCUSDT")
+print(position)
